5ef44e139f1781495c4ee9030c8bcba2951c71a1
Parth <Parthpurbia@gmail.com>
2025-06-17 18:20:46 +0530

    Modification

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,4 @@ echo "Stopping old container if running.."
 docker rm -f static-site-container 2>/dev/null
 
 echo "Running new container"
-docker run -d --name static-site-container -p 8090:80 static-site:v1
+docker run -d --name static-site-container -p 8888:80 static-site:v1
