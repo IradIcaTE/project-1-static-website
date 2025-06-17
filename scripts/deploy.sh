8cd0649c2d396b9a0a98c7ff7c3fca953b3b87e1
Parth <Parthpurbia@gmail.com>
2025-06-17 19:03:16 +0530

    Modification

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+cd docker   # go into docker folder
 echo "Building Docker Image"
-docker build -t static-site:v1 docker
+docker build -t static-site:v1 .
 
 echo "Stopping old container if running.."
 docker rm -f static-site-container 2>/dev/null
 
 echo "Running new container"
-docker run -d --name static-site-container -p 8888:80 static-site:v1
+docker run -d --name static-site-container -p 8080:80 static-site:v1
